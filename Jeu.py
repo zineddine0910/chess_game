a5e5851cfc19ecef6e6e7b632acf190691f22c78
AZEM Amar <amarazem33@gmail.com>
2023-01-26 13:38:42 +0100

    roi en echec

--- Jeu.py
+++ Jeu.py
@@ -1,10 +1,10 @@
 import pygame
 import Plateau
+import copy
 
 def main():
 
     grille = Plateau.Plateau()
-    grille.affichage()
 
     # for i in range(8):
     #     for j in range(8):
@@ -13,43 +13,82 @@ def main():
     # print(grille.coups_possibles((0,1)))
 
     pygame.init()
-    ecran = pygame.display.set_mode((640,640))
+    ecran = pygame.display.set_mode((640,700))
     case1 = None
     case2 = None
     joueur_actuel = "blanc"
+    choix = []
+    font = pygame.font.Font(None, 30)
+    button_rect = pygame.Rect(0, 650, 30, 30)
     boucle_du_jeu = True
+    
+    
     while boucle_du_jeu:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 boucle_du_jeu = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                x, y = event.pos
-                x = x//80
-                y = y//80
-                # print("Coordonnées de la souris: ", y, x)
-                # print(grille.coups_possibles((y,x))) 
-                if case1 == None:
-                    case1 = y, x
+                if button_rect.collidepoint(event.pos):
+                # Effectuer une action lorsque le bouton est cliqué
+                    grille.annuler_coup()
+                    grille.annuler_coup()
+                    continue
                 else:
-                    case2 = y, x
-                print(case1)
-                print(case2)
-                print("----------")
-                if (case1 != None and case2 != None):
-                    if(joueur_actuel == grille.affiche_couleur(case1)):
-                        if(case2 in grille.coups_possibles(case1)):
+                    x, y = event.pos
+                    x = x//80
+                    y = y//80
+                    if(x < 0 or y < 0 or x > 7 or y > 7): 
+                        continue
+                    # print("Coordonnées de la souris: ", y, x)
+                    # print(grille.coups_possibles((y,x))) 
+                    if case1 == None:
+                        case1 = (y, x)
+                        if(grille.affiche_couleur(case1) != joueur_actuel):
+                            print("mauvais_joueur")
+                            case1 = None
+                        else:                     
+                            choix = grille.coups_possibles(case1)
+                            if (not choix):
+                                case1 = None
+                            if(grille.getGrille()[y][x].getNom() == "roi"):
+                                grille.enleve_cases_roi(choix, grille.affiche_couleur(case1))
+                    else:
+                        case2 = y, x
+                    # print(case1)
+                    # print(case2)
+                    # print("----------")
+                    if (case1 != None and case2 != None):
+                        if(case2 in choix):
                             grille.coup(case1, case2)
-                            grille.affichePlateau(ecran)
-                            if joueur_actuel == "blanc":
-                                joueur_actuel = "noir"
-                            else: joueur_actuel = "blanc"
+                            if(grille.roi_en_echec("noir") == True):
+                                print("le roi noir est en echec")
+                            joueur_actuel = "noir" 
+                            grille.coup_IA()
+                            if(grille.roi_en_echec("blanc") == True):
+                                print("le roi blanc est en echec")
+                            joueur_actuel = "blanc"   
                         else: print("jamais")
-                    else: print("mauvais_joueur")
-                    case1 = None
-                    case2 = None
+                        choix = []
+                        case1 = None
+                        case2 = None
+                    
         ecran.fill((205, 133, 63))
-        grille.affichePlateau(ecran)
-        #grille.change_couleur(ecran)
+        grille.affichePlateau(ecran, choix)
+        grille.verif_echec_et_mat_noir()
+        if( grille.echec_et_mat_noir == True):
+            text = font.render("Les blancs ont gagné", True, (0, 0, 0))
+            ecran.blit(text, (200, 650))
+            pygame.event.set_blocked(pygame.MOUSEBUTTONDOWN)
+        grille.verif_echec_et_mat_blanc()
+        if(grille.echec_et_mat_blanc == True):
+            text = font.render("Les noirs ont gagné", True, (0, 0, 0))
+            ecran.blit(text, (200, 650))
+            pygame.event.set_blocked(pygame.MOUSEBUTTONDOWN)
+            
+        pygame.draw.rect(ecran, (0, 0, 0), button_rect)
+        button_text = font.render("", True, (255, 255, 255))
+        ecran.blit(button_text, (0, 750))
         pygame.display.update()
 
     pygame.quit()
--- Plateau.py
+++ Plateau.py
@@ -1,10 +1,13 @@
 import Piece
 import pygame
+import random
 
 class Plateau:
 
     grille = [[None for i in range(8)] for j in range(8)]
-
+    historique = []
+    echec_et_mat_blanc = False
+    echec_et_mat_noir = False
     def __init__(self):
 
         # initialisation pieces blanches
@@ -35,6 +38,7 @@ class Plateau:
             self.grille[1][i] = Piece.Piece("pion", "noir")
             self.grille[6][i] = Piece.Piece("pion", "blanc")
 
+        #self.grille[5][4] = Piece.Piece("pion", "noir")
         # self.grille[1][1] = None
         # self.grille[6][4] = None
         # self.grille[0][1] = Piece.Piece("reine", "blanc")
@@ -42,11 +46,16 @@ class Plateau:
     def getGrille(self):
         return self.grille
 
-    def affichePlateau(self,ecran):
+
+    def affichePlateau(self,ecran, choix):
         for i in range(8):
             for j in range(8):
                 if ((i%2 == 0) and (j%2 == 0)) or ((i%2 == 1) and (j%2 == 1)):
                     pygame.draw.rect(ecran, (255, 255, 255), (i*80, j*80, 80, 80))
+                if((j, i) in choix):
+                    pygame.draw.circle(ecran, (0, 255, 0), (i*80+40, j*80+40), 15)
+                
+                    
 
         for i in range(8):
             for j in range(8):
@@ -65,9 +74,6 @@ class Plateau:
                     ecran.blit(image, (j*80+20, i*80+8))
         pygame.display.flip()
 
-    def change_couleur(self, ecran):
-        pygame.draw.rect(ecran, (255, 0, 0), (80, 80, 80, 80))
-        print("yoo")
         
     def affichage(self):
         for i in range(8):
@@ -82,51 +88,40 @@ class Plateau:
 
     def coups_possibles_pion(self,case,tab_coups):
         # pions blancs
-        if self.grille[case[0]][case[1]].getCouleur() == "blanc":
-            try:
-                if(case[0]-1 > -1 and self.grille[case[0]-1][case[1]+1] != None and self.grille[case[0]-1][case[1]+1].getCouleur() == "noir"):
-                    tab_coups.append((case[0]-1,case[1]+1))
-            except IndexError:
-                pass
-            try:
-                if(case[0]-1 > -1 and case[1]-1 > -1 and self.grille[case[0]-1][case[1]-1] != None and self.grille[case[0]-1][case[1]+1].getCouleur() == "noir"):
-                    tab_coups.append((case[0]-1,case[1]-1))
-            except IndexError:
-                pass
-            if (case[0] == 6):
-                if(self.grille[case[0]-1][case[1]] == None):
-                    tab_coups.append((case[0]-1,case[1]))
-                if(self.grille[case[0]-1][case[1]] == None and self.grille[case[0]-2][case[1]] == None):
-                    tab_coups.append((case[0]-2,case[1]))
-            else:
-                try:
-                    if(self.grille[case[0]-1][case[1]] == None):
-                        tab_coups.append((case[0]-1,case[1]))
-                except IndexError:
-                    pass
-        # pions noirs
-        if self.grille[case[0]][case[1]].getCouleur() == "noir":
-            try:
-                if(self.grille[case[0]+1][case[1]+1] != None and self.grille[case[0]+1][case[1]+1].getCouleur() == "blanc"):
-                    tab_coups.append((case[0]+1,case[1]+1))
-            except IndexError:
-                pass
-            try:
-                if(case[1]-1 > -1 and self.grille[case[0]+1][case[1]-1] != None and self.grille[case[0]+1][case[1]+1].getCouleur() == "blanc"):
-                    tab_coups.append((case[0]+1,case[1]-1))
-            except IndexError:
-                pass
-            if (case[0] == 1):
-                if(self.grille[case[0]+1][case[1]] == None):
-                    tab_coups.append((case[0]+1,case[1]))
-                if(self.grille[case[0]+1][case[1]] == None and self.grille[case[0]+2][case[1]] == None):
-                    tab_coups.append((case[0]+2,case[1]))
-            else:
-                try:
-                    if(self.grille[case[0]+1][case[1]] == None):
-                        tab_coups.append((case[0]+1,case[1]))
-                except IndexError:
-                    pass
+        x, y = case[0], case[1]
+        if self.grille[x][y].getCouleur() == "blanc":
+            if x > 0:
+                if (self.grille[x-1][y] == None):
+                    tab_coups.append((x-1, y))
+            if x == 6:
+                if (self.grille[x-1][y] == None and self.grille[x-2][y] == None):
+                    tab_coups.append((x-2, y))
+            if (x > 0 and y > 0):
+                if (self.grille[x-1][y-1] != None):
+                    if (self.grille[x-1][y-1].getCouleur() == "noir"):
+                        tab_coups.append((x-1, y-1))
+            
+            if (x > 0 and y < 7):
+                if (self.grille[x-1][y+1] != None):
+                    if (self.grille[x-1][y+1].getCouleur() == "noir"):
+                        tab_coups.append((x-1, y+1))
+                        
+        else:
+            if x < 7:
+                if (self.grille[x+1][y] == None):
+                    tab_coups.append((x+1, y))
+            if x == 1:
+                if (self.grille[x+1][y] == None and self.grille[x+2][y] == None):
+                    tab_coups.append((x+2, y))
+            if (x < 7 and y > 0):
+                if (self.grille[x+1][y-1] != None):
+                    if (self.grille[x+1][y-1].getCouleur() == "blanc"):
+                        tab_coups.append((x+1, y-1))
+                
+            if (x < 7 and y < 7):
+                if (self.grille[x+1][y+1] != None):
+                    if (self.grille[x+1][y+1].getCouleur() == "blanc"):
+                        tab_coups.append((x+1, y+1))
 
     def coups_possibles_tour(self,case,tab_coups):
         # droite
@@ -213,89 +208,29 @@ class Plateau:
                 break
 
     def coups_possibles_cavalier(self,case,tab_coups):
-    # partie gauche 
-        if self.grille[case[0]-2][case[1]-1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-2][case[1]-1].getCouleur():
-            if case[0]-2 > -1 and case[1]-1 > -1 :
-                tab_coups.append((case[0]-2,case[1]-1))
-        if self.grille[case[0]-1][case[1]-2] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-1][case[1]-2].getCouleur():
-            if case[0]-2 > -1 and case[1]-2 > -1 :
-                tab_coups.append((case[0]-1,case[1]-2))
-        try: 
-            if self.grille[case[0]+1][case[1]-2] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+1][case[1]-2].getCouleur():
-                if case[1]-2 > -1 :
-                    tab_coups.append((case[0]+1,case[1]-2))
-        except IndexError:
-            pass
-        try:
-            if self.grille[case[0]+2][case[1]-1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+2][case[1]-1].getCouleur():
-                if case[1]-1 > -1 :
-                    tab_coups.append((case[0]+2,case[1]-1))
-        except IndexError:
-            pass
-        # partie droite
-        try:
-            if self.grille[case[0]-2][case[1]+1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-2][case[1]+1].getCouleur():
-                if case[0]-2 > -1 :
-                    tab_coups.append((case[0]-2,case[1]+1))
-        except IndexError:
-            pass
-        try:
-            if self.grille[case[0]-1][case[1]+2] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-1][case[1]+2].getCouleur():
-                if case[0]-1 > -1 :
-                    tab_coups.append((case[0]-1,case[1]+2))
-        except IndexError:
-            pass
-        try:
-            if self.grille[case[0]+1][case[1]+2] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+1][case[1]+2].getCouleur():
-                tab_coups.append((case[0]+1,case[1]+2))
-        except IndexError:
-            pass
-        try:
-            if self.grille[case[0]+2][case[1]+1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+2][case[1]+1].getCouleur():
-                tab_coups.append((case[0]+2,case[1]+1))
-        except IndexError:
-            pass
+        x, y = case[0], case[1]
+        for i in [-2, -1, 1, 2]:
+            for j in [-2, -1, 1, 2]:
+                if abs(i) != abs(j):
+                    if 0 <= x + i < 8 and 0 <= y + j < 8:
+                        if self.grille[x+i][y+j] == None:
+                            tab_coups.append((x + i, y + j))
+                        else:
+                            if self.grille[x][y].getCouleur() != self.grille[x+i][y+j].getCouleur():
+                                tab_coups.append((x + i, y + j))
+        return tab_coups
     
     def coups_possibles_roi(self,case,tab_coups):
-        # haut
-        if self.grille[case[0]-1][case[1]-1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-1][case[1]-1].getCouleur():
-            if case[0]-1 > -1 and case[1]-1 > -1 :
-                tab_coups.append((case[0]-1,case[1]-1))
-        if self.grille[case[0]-1][case[1]] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-1][case[1]].getCouleur():
-            if case[0]-1 > -1 :
-                tab_coups.append((case[0]-1,case[1]))
-        try:
-            if self.grille[case[0]-1][case[1]+1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]-1][case[1]+1].getCouleur():
-                if case[0]-1 > -1 :
-                    tab_coups.append((case[0]-1,case[1]+1))
-        except IndexError:
-            pass
-        # gauche + droite
-        if self.grille[case[0]][case[1]-1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]][case[1]-1].getCouleur():
-            if case[1]-1 > -1 :
-                tab_coups.append((case[0],case[1]-1))
-        try:
-            if self.grille[case[0]][case[1]+1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]][case[1]+1].getCouleur():
-                    tab_coups.append((case[0],case[1]+1))
-        except IndexError:
-            pass
-        # bas
-        try:
-            if self.grille[case[0]+1][case[1]-1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+1][case[1]-1].getCouleur():
-                if case[1]-1 > -1 :
-                    tab_coups.append((case[0]+1,case[1]-1))
-        except IndexError:
-            pass
-        try:
-            if self.grille[case[0]+1][case[1]] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+1][case[1]].getCouleur():
-                tab_coups.append((case[0]+1,case[1]))
-        except IndexError:
-            pass
-        try:
-            if self.grille[case[0]+1][case[1]+1] == None or self.grille[case[0]][case[1]].getCouleur() != self.grille[case[0]+1][case[1]+1].getCouleur():
-                tab_coups.append((case[0]+1,case[1]+1))
-        except IndexError:
-            pass
+        x, y = case[0], case[1]
+        for i in [-1, 0, 1]:
+            for j in [-1, 0, 1]:
+                if (i != 0 or j != 0):
+                    if 0 <= x + i < 8 and 0 <= y + j < 8:
+                        if self.grille[x+i][y+j] == None:
+                            tab_coups.append((x + i, y + j))
+                        else:
+                            if self.grille[x][y].getCouleur() != self.grille[x+i][y+j].getCouleur():
+                                tab_coups.append((x + i, y + j))
 
     def coups_possibles(self,case):
 
@@ -334,11 +269,121 @@ class Plateau:
 
 
     def coup(self,case1,case2):
+        
+        piece_case2 = self.grille[case2[0]][case2[1]]
+        self.historique.append((case1, case2, piece_case2))
         self.grille[case2[0]][case2[1]] = self.grille[case1[0]][case1[1]]
         self.grille[case1[0]][case1[1]] = None
     
+    def annuler_coup(self):
+        if self.historique:
+            ancienne_case, nouvelle_case, piece = self.historique.pop()
+            self.grille[ancienne_case[0]][ancienne_case[1]] = self.grille[nouvelle_case[0]][nouvelle_case[1]]
+            self.grille[nouvelle_case[0]][nouvelle_case[1]] = piece
     def affiche_couleur(self,case):
         if self.grille[case[0]][case[1]] == None:
             return "case vide"
         else:
             return self.grille[case[0]][case[1]].getCouleur()
+    
+    # def aucun_coup_dispo_IA(self, list):
+        
+    #     for i in range(len(list)):
+    #         if self.coups_possibles(list[i]) != []:
+    #             return False
+    #     return True
+    # def coup_IA(self):
+    #     list = []
+    #     list2 = []
+    #     for i in range(8):
+    #             for j in range(8):
+    #                 if self.grille[i][j] != None:
+    #                     if self.grille[i][j].getCouleur() == "noir":
+    #                         list.append((i, j))
+        
+    #     while(not list2):
+    #         case = random.choice(list)
+    #         list2 = self.coups_possibles(case)
+    #         if(self.grille[case[0]][case[1]].getNom() == "roi"):
+    #             self.enleve_cases_roi(list2, "noir")
+    #         if(self.aucun_coup_dispo_IA(list) == True):
+    #             self.echec_et_mat_noir = True
+    #             return
+    #     case2 = random.choice(list2)
+    #     self.coup(case, case2)
+        
+    def coup_IA(self):
+        list = []
+        list2 = []
+        for i in range(8):
+                for j in range(8):
+                    if self.grille[i][j] != None:
+                        if self.grille[i][j].getCouleur() == "noir":
+                            list.append((i, j))
+        
+        while(not list2):
+            for i in range(8):
+                for j in range(8):
+                    if self.grille[i][j] != None:
+                        if self.grille[i][j].getCouleur() == "noir":
+                            list.append((i, j))
+            case = random.choice(list)
+            list2 = self.coups_possibles(case)
+        case2 = random.choice(list2)
+        self.coup(case, case2)
+    
+    def roi_en_echec(self, couleur): #verifie si un des rois est en echec
+        x,y = -1,-1
+        for i in range(8):
+            for j in range(8):
+                if (self.grille[i][j] != None and self.grille[i][j].getNom() == "roi" and self.grille[i][j].getCouleur() == couleur):
+                    x, y = i, j
+        for i in range(8):
+            for j in range(8):
+                if (self.grille[i][j] != None and self.grille[i][j].getCouleur() != couleur):
+                    if((x,y) in self.coups_possibles((i,j))):
+                        return True
+        return False
+    
+    def enleve_cases_roi(self, tab_coups, couleur): #nleve les cases qui representeraient un echec et mat pour le roi
+        cpt = 0
+        arret = False
+        while not arret:
+            if self.case_vide_en_echec(tab_coups[cpt], couleur):
+                tab_coups.remove(tab_coups[cpt])
+            else: cpt = cpt+1
+            if cpt == len(tab_coups):
+                arret = True
+                    
+    def case_vide_en_echec(self, case, couleur): #verifie si une case vide sera en echecs si elle reçoit une piece de couleur couleur
+        x,y = case[0], case[1]
+        for i in range(8):
+            for j in range(8):
+                if (self.grille[i][j] != None and self.grille[i][j].getNom() != "pion" and self.grille[i][j].getCouleur() != couleur):
+                    if((x,y) in self.coups_possibles((i,j))):
+                        return True
+        if(couleur == "blanc"):
+            if(self.grille[x-1][y-1] != None and self.grille[x-1][y-1].getNom() == "pion" and self.grille[x-1][y-1].getCouleur() == "noir"):
+                return True
+            if(self.grille[x-1][y+1] != None and self.grille[x-1][y+1].getNom() == "pion" and self.grille[x-1][y+1].getCouleur() == "noir"):
+                return True
+        else:
+            if(self.grille[x+1][y-1] != None and self.grille[x+1][y-1].getNom() == "pion" and self.grille[x+1][y-1].getCouleur() == "blanc"):
+                return True
+            if(self.grille[x+1][y+1] != None and self.grille[x+1][y+1].getNom() == "pion" and self.grille[x+1][y+1].getCouleur() == "blanc"):
+                return True
+        return False
+                    
+    def verif_echec_et_mat_blanc(self): #verifie si les blanc ont perdu
+        for i in range(8):
+            for j in range(8):
+                if (self.grille[i][j] != None and self.grille[i][j].getNom() == "roi" and self.grille[i][j].getCouleur() == "blanc"):
+                    return
+        self.echec_et_mat_blanc = True
+    
+    def verif_echec_et_mat_noir(self): #verifie si les noirs ont perdu
+        for i in range(8):
+            for j in range(8):
+                 if (self.grille[i][j] != None and self.grille[i][j].getNom() == "roi" and self.grille[i][j].getCouleur() == "noir"):
+                    return
+        self.echec_et_mat_noir = True
